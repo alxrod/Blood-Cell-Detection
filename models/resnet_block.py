bc7b9135b88574addcb619efcbdb8e874acfa6d5
Alex <alexbrodriguez@gmail.com>
2020-02-02 13:06:28 -0500

    v1 resnet, still needs debug

--- models/resnet_block.py
+++ models/resnet_block.py
@@ -1,32 +1,49 @@
-from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import Conv2D
-from keras.layers.convol
+import tensorflow as tf
+
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Activation                          
+from tensorflow.keras.layers import Dense                                 
+from tensorflow.keras.models import Model                             
+from tensorflow.keras.layers import add                                   
+from tensorflow.keras.regularizers import l2                           
+
 class ResnetBlock(tf.keras.Model):
-	def __init__(self, filters, kernel_size):
-		super(ResnetBlock, self).__init__(name="")
-		filters1, filters2, filters3 = filters
+	def __init__(self, K, stride, chanDim, red=False, reg=0.0001, bnEps=2e-5, bnMom=0.9):
+            super(ResnetBlock, self).__init__()
+            self.red = red
+            self.bn1 = BatchNormalization(axis=chanDim, epsilon=bnEps, momentum=bnMom)
+            self.act1 = Activation("relu")
+            self.conv1 = Conv2D(int(K*0.25), (1,1), use_bias=False, kernel_regularizer=l2(reg))
+            
+            self.bn2 = BatchNormalization(axis=chanDim, epsilon=bnEps, momentum=bnMom)
+            self.act2 = Activation("relu")
+            self.conv2 = Conv2D(int(K*0.25), (3,3), strides=stride, padding="same", use_bias=False, kernel_regularizer=l2(reg))
 
-		self.conv1 = tf.keras.layers.Conv2D(filters1, (1,1))
-		self.bn1 = tf.keras.layers.BatchNormalization()
-		
-		self.conv2 = tf.keras.layers.Conv2D(filters2, kernel_size, padding="same")
-		self.bn2 = tf.keras.layers.BatchNormalization()
+            self.bn3 = BatchNormalization(axis=chanDim, epsilon=bnEps, momentum=bnMom)
+            self.act3 = Activation("relu")
+            self.conv3 = Conv2D(K, (1,1), use_bias=False, kernel_regularizer=l2(reg))
+            self.scConv = Conv2D(K, (1,1), strides=stride, use_bias=False, kernel_regularizer=l2(reg))
 
-		self.conv3 = tf.keras.layers.Conv2D(filters3, (1,1))
-		self.bn3 = tf.keras.layers.BatchNormalization()
-		
-	
-	def call(self, input_tensor, training=False):
-		x = self.conv1(input_tensor)
-		x = self.bn1(x, training=training)
-		x = tf.nn.relu(x)
 
-		x = self.conv2(x)
-		x = self.bn2(x, training=training)
-		x = tf.nn.relu(x)
-		
-		x = self.conv3(x)
-		x = self.bn3(x, training=training)
-	
-		x = x + input_tensor
-		return tf.nn.relu(x)
+	def call(self, data, training=False):
+            shortcut = data
+            x = self.bn1(data)
+            act1 = self.act1(x)
+            x = self.conv1(act1)
+
+            x = self.bn2(x)
+            x = self.act2(x)
+            x = self.conv2(x)
+
+            x = self.bn3(x)
+            x = self.act3(x)
+            x = self.conv3(x)
+            
+            if self.red:
+                shortcut = self.scConv(act1)
+
+            x = add([x, shortcut])
+            return x
+
+            
--- models/resnet_model.py
+++ models/resnet_model.py
@@ -1,42 +1,75 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Dropout
-from tensorflow.keras import Model
+# import the necessary packages
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import ZeroPadding2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import add
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras import backend as K
 from resnet_block import ResnetBlock
-from load_data import load_folder
+
 
 class ResnetModel(Model):
-	def __init__(self):
-		super(ResnetModel, self).__init__()
-		
-		self.conv1 = Conv2D(32, 3, padding="same", activation="relu", kernel_regularizer=tf.keras.regularizers.l1(0.4))
-		self.pool1 = MaxPool2D((2,2))
-		self.conv2 = Conv2D(64, 3, padding="same", activation="relu", kernel_regularizer=tf.keras.regularizers.l1(0.4))
-		self.pool2 = MaxPool2D((2,2))
-		self.res1 = ResnetBlock([32, 32, 64], 1)
-		self.res2 = ResnetBlock([32, 32, 64], 1)
-		self.flatten = Flatten()
-		self.d1 = Dense(512, activation="relu")
-		self.dropout1 = Dropout(0.8)
-		self.d2 = Dense(128, activation="relu")
-		self.dropout2 = Dropout(0.8)
-		self.d3 = Dense(32, activation="relu")
-		self.dropout3 = Dropout(0.8)
-		self.d4 = Dense(4, activation="softmax")
+    def __init__(self):
+        super(ResnetModel, self).__init__()
+        self.stages = (3,4,6)
+        self.filters = (64, 128, 256, 512)
 
-	def call(self, x):
-		x = self.conv1(x)
-		x = self.pool1(x)
-		x = self.conv2(x)
-		x = self.pool2(x)
-		x = self.res1(x, training=True)
-		x = self.res2(x, training=True)
-		x = self.flatten(x)
-		x = self.d1(x)
-		x = self.dropout1(x)
-		x = self.d2(x)
-		x = self.dropout2(x)
-		x = self.d3(x)
-		x = self.dropout3(x)
-		x = self.d4(x)
-		return x
+        self.reg = 0.0001
+        self.bnEps=2e-5
+        self.bnMom=0.9
+            
+            #I hardcoded this one
+        self.inputShape = (112,112,3)
+        self.chanDim = -1
+        
+        self.bn0 = BatchNormalization(axis=self.chanDim, epsilon=self.bnEps, momentum=self.bnMom)
+        self.conv1 = Conv2D(self.filters[0], (5,5), use_bias=False, padding="same", kernel_regularizer=l2(self.reg))
+        self.bn1 = BatchNormalization(axis=self.chanDim, epsilon=self.bnEps, momentum=self.bnMom)
+        self.act1 = Activation("relu")
+        self.zp1 = ZeroPadding2D((1,1))
+        self.mp1 = MaxPooling2D((3,3), strides=(2,2))
+        self.resBlocks = []
+        for i in range(0, len(self.stages)):
+            stride = (1,1) if i == 0 else (2,2)
+            rb = ResnetBlock(self.filters[i+1], stride, self.chanDim, red=True, bnEps=self.bnEps, bnMom=self.bnMom)
+            self.resBlocks.append(rb)
 
+            for j in range(0, self.stages[i]-1):
+                rb = ResnetBlock(self.filters[i+1], (1,1), self.chanDim, bnEps=self.bnEps, bnMom=self.bnMom)
+                self.resBlocks.append(rb)
+        
+        self.bn2 = BatchNormalization(axis=self.chanDim, epsilon=self.bnEps, momentum=self.bnMom)
+        self.act2 = Activation("relu")
+        self.ap1 = AveragePooling2D((8, 8))
+
+        self.f = Flatten()
+        self.d1 = Dense(4, kernel_regularizer=l2(self.reg))
+        self.act3 = Activation("softmax")
+
+    def call(self, inputData):
+        x = self.bn0(inputData)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.act1(x)
+        x = self.zp1(x)
+        x = self.mp1(x)
+
+        for rb in self.resBlocks:
+            print("Res Block #" +str(self.resBlocks.index(rb)))
+            x = rb(x, training=True)
+
+        x = self.bn2(x)
+        x = self.act2(x)
+        x = self.ap1(x)
+
+        x = self.f(x)
+        x = self.d1(x)
+        return self.act3(x)
